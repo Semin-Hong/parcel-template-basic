fc0b580266869a2708252b35b5108bfdc8ffbc49
Semin-Hong <hongsem0103@gmail.com>
2022-03-28 16:39:53 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// ESM -- 일반 js
+// CommonJS -- nodejs 
+
+// import autoprefixer from 'autoprefixer'
+
+
+// export {
+//  plugins: [
+//     autoprefixer
+//  ]
+// }
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
